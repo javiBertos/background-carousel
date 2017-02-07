872551e8bd97d37cc42cfc53746ea1a1832dda40
Javier Bertos <yo@javierbertos.name>
2017-02-07 23:59:12 +0100

    Minor updates

--- app/app.js
+++ app/app.js
@@ -1,8 +1,8 @@
-(() => {
-    let carousel = document.getElementsByTagName('section')[0], current = 1;
-    window.setInterval(() => {
-        carousel.classList.remove(`bg-${current}`);
+(function () {
+    var carousel = document.getElementsByTagName('section')[0], current = 1;
+    window.setInterval(function () {
+        carousel.classList.remove("bg-" + current);
         current = ++current == 4 ? 1 : current;
-        carousel.classList.add(`bg-${current}`);
+        carousel.classList.add("bg-" + current);
     }, 5000);
 })();
